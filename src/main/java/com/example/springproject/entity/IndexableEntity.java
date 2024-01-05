395498593f45f40857311a06276cc45d09c11fb1
mariohieu09 <trantrunghieu20092001@gmail.com>
2024-01-05 17:26:37 +0700

    them generated value cho IndexableEntity

--- src/main/java/com/example/springproject/entity/IndexableEntity.java
+++ src/main/java/com/example/springproject/entity/IndexableEntity.java
@@ -1,10 +1,13 @@
 package com.example.springproject.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class IndexableEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
