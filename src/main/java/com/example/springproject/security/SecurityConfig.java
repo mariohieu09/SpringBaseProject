7fa95236efa5948c9eee4669b932b239ae31d6c3
mariohieu09 <trantrunghieu20092001@gmail.com>
2024-01-05 17:03:46 +0700

    config file properties and security

--- src/main/java/com/example/springproject/security/SecurityConfig.java
+++ src/main/java/com/example/springproject/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springproject.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
