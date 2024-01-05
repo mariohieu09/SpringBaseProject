11428eaf9c4459d6b1f0bf70aee17293dc8b2a40
mariohieu09 <trantrunghieu20092001@gmail.com>
2024-01-05 17:17:45 +0700

    adding cors config security, generated value to IndexableEntity, test creating api

--- src/main/java/com/example/springproject/security/SecurityConfig.java
+++ src/main/java/com/example/springproject/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springproject.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +15,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(s -> s.anyRequest().permitAll()).
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf().disable()
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).
                 build();
     }
 
