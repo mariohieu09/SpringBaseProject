b78bb034ca6c57d57b29f4828bc9a970ef54a4ed
mariohieu09 <trantrunghieu20092001@gmail.com>
2024-01-05 08:25:46 +0700

    config permit all http requests

--- src/main/java/com/example/springproject/security/SecurityConfig.java
+++ src/main/java/com/example/springproject/security/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.example.springproject.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(s -> s.anyRequest().permitAll()).
+                build();
+    }
 }
