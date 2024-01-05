620b6ab5386f74c0840c1248c3637724618d56f5
mariohieu09 <trantrunghieu20092001@gmail.com>
2024-01-05 17:25:21 +0700

    chinh sua security, them bean password encoder

--- src/main/java/com/example/springproject/security/SecurityConfig.java
+++ src/main/java/com/example/springproject/security/SecurityConfig.java
@@ -1,16 +1,30 @@
 package com.example.springproject.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(s -> s.anyRequest().permitAll()).
-                build();
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf().disable()
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
