8c17345e5e4c3f6444e544600cd1acb661cc6ee0
mariohieu09 <trantrunghieu20092001@gmail.com>
2024-01-05 17:40:50 +0700

    adding ErrorMessage, handle Not found exception

--- src/main/java/com/example/springproject/advice/BaseControllerAdvice.java
+++ src/main/java/com/example/springproject/advice/BaseControllerAdvice.java
@@ -1,4 +1,22 @@
 package com.example.springproject.advice;
 
+import com.example.springproject.exception.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
 public class BaseControllerAdvice {
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage handleNotFoundException(Exception ex, WebRequest request){
+        return ErrorMessage.builder()
+                .statusCode(404)
+                .message(ex.getMessage())
+                .build();
+    }
 }
